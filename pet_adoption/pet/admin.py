27f8bd5df2ac6149f690b4d0b67d69b611ec80be
MariuszOwczarek <89746880+MariuszOwczarek@users.noreply.github.com>
2023-08-20 17:31:20 +0200

    Update database, add static

--- pet_adoption/pet/admin.py
+++ pet_adoption/pet/admin.py
@@ -1,23 +1,22 @@
 from django.contrib import admin
-from .models import Animal, UserProfile, Adoption, Treatments, Services
+from .models import Animal, UserProfile, Adoption, Treatment, Service
 
 
 @admin.register(Animal)
 class AnimalAdmin(admin.ModelAdmin):
-    list_display = ["name", "species","breed","colour", "chip", "available_for_adoption"]
-    list_filter = ["breed","colour","available_for_adoption"]
+    list_display = ["name", "species","breed","colour", "chip", "available_to_adoption"]
+    list_filter = ["breed","colour","available_to_adoption"]
     search_fields = ["species"]
+
 
 
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ["name", "surname"]
+    list_display = ["first_name", "last_name"]
     list_filter = ["id"]
     search_fields = ["surname"]
 
 
-admin.site.register(UserProfile)
-admin.site.register(Animal)
 admin.site.register(Adoption)
-admin.site.register(Treatments)
-admin.site.register(Services)
+admin.site.register(Treatment)
+admin.site.register(Service)
