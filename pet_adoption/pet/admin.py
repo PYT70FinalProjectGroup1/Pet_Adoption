6316eb6c5a211fb126546205eb41df13621fb03b
MariuszOwczarek <89746880+MariuszOwczarek@users.noreply.github.com>
2023-08-20 13:09:01 +0200

    example admin

--- pet_adoption/pet/admin.py
+++ pet_adoption/pet/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import Pet, People
 
-admin.site.register(Pet)
-admin.site.register(People)
+
+@admin.register(Pet)
+class PetAdmin(admin.ModelAdmin):
+    list_display = ["name", "species","breed","color", "chip", "available_for_adoption"]
+    list_filter = ["breed","color","available_for_adoption"]
+    search_fields = ["species"]
+
+@admin.register(People)
+class PeopleAdmin(admin.ModelAdmin):
+    list_display = ["name", "surname"]
+    list_filter = ["id"]
+    search_fields = ["surname"]
--- pet_adoption/pet/models.py
+++ pet_adoption/pet/models.py
@@ -9,19 +9,21 @@ class BaseModel(models.Model):
 
     class Meta:
         """ Meta class"""
-
         abstract = True
 
 
 class Pet(BaseModel):
     """ Pet Model """
 
-    name = models.CharField(max_length=32, null=False)
-    colour = models.CharField(max_length=64, null=False)
+    name = models.CharField(max_length=32, null=False)    
     species = models.CharField(max_length=9, null=False)
+    breed = models.CharField(max_length=40, null=False)
+    colour = models.CharField(max_length=64, null=False)
     chip = models.CharField(max_length=64, unique=True, null=False)
+    description = models.CharField(max_length=500, null=False)
     registration = models.DateField()
     about_pet = models.TextField()
+    available_for_adoption = models.BooleanField(default=True)
     image = models.ImageField(upload_to='image/')
 
     def __str__(self):
@@ -29,11 +31,11 @@ class Pet(BaseModel):
 
 
 class People(BaseModel):
-    """ Class Peaple """
+    """ Class People """
 
     name = models.CharField(max_length=32, null=False)
     surname = models.CharField(max_length=64, null=False)
-    id_client = models.CharField(max_length=64, unique=True, null=False)
+    #id_client = models.CharField(max_length=64, unique=True, null=False)  kazdy bedzie mial swoje id
     mail = models.EmailField(max_length=64, null=True)
     phone = models.CharField(max_length=32, null=False, unique=True)
     street = models.CharField(max_length=64, null=False)
