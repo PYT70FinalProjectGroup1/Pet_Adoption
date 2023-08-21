7483d533907ea69d91e2166336681f1189438476
MariuszOwczarek <89746880+MariuszOwczarek@users.noreply.github.com>
2023-08-21 13:43:55 +0200

    donate page

--- pet_adoption/pet/urls.py
+++ pet_adoption/pet/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import HomeView, RegisterView, AboutView
+from .views import HomeView, RegisterView, AboutView, DonateView
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
     path("about", AboutView.as_view(), name="about"),
+    path("donate", DonateView.as_view(), name="donate"),
 
 
     path("accounts/login/", auth_views.LoginView.as_view(), name="login"),
