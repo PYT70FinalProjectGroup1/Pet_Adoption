79ef0fe6e6f48ab023281228cf39b67e5c608bc7
MariuszOwczarek <89746880+MariuszOwczarek@users.noreply.github.com>
2023-08-21 12:38:43 +0200

    about page

--- pet_adoption/pet/views.py
+++ pet_adoption/pet/views.py
@@ -23,3 +23,11 @@ class HomeView(LoginRequiredMixin,TemplateView):
         context["animals"] = animals
 
         return context
+
+
+class AboutView(LoginRequiredMixin,TemplateView):
+    template_name = 'about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
