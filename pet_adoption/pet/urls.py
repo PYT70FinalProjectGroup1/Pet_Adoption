ef4f6cdc7914e1142111d24e0432dc80f6d1aaf7
MariuszOwczarek <89746880+MariuszOwczarek@users.noreply.github.com>
2023-08-24 16:02:19 +0200

    Add adoption_stories

--- pet_adoption/pet/urls.py
+++ pet_adoption/pet/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import HomeView, AboutView, DonateView, AdoptedAnimalsView, AvailableServicesView, CreateServiceView, \
+from .views import HomeView, AboutView, DonateView, MyPetsView, AvailableServicesView, CreateServiceView, \
     AvailableTreatmentsView, CreateTreatmentView, FindCatsView, FindDogsView, FindOtherPetsView, FindAllView, \
-    RegisterView, AnimalDetailView, CreateAdoptionView
+    RegisterView, AnimalDetailView, CreateAdoptionView, AdoptionStoriesView
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
@@ -10,7 +10,7 @@ urlpatterns = [
     path("donate", DonateView.as_view(), name="donate"),
     path('animal_detail/<int:animal_id>/', AnimalDetailView.as_view(), name='animal_detail'),
 
-    path("adopted_animals/", AdoptedAnimalsView.as_view(), name="adopted_animals"),
+    path("my_pets/", MyPetsView.as_view(), name="my_pets"),
     
     path("services/<int:pk>/", AvailableServicesView.as_view(), name="services"),
     path("create_service/<int:animal_id>/",CreateServiceView.as_view(),name="create_service"),
@@ -22,6 +22,8 @@ urlpatterns = [
     path("find/dogs", FindDogsView.as_view(), name="find_dogs"),
     path("find/other_pets", FindOtherPetsView.as_view(), name="find_other_pets"),
     path("find/all_pets", FindAllView.as_view(), name="find_all_pets"),
+
+    path("adoption_stories", AdoptionStoriesView.as_view(), name="adoption_stories"),
     
     path("accounts/login/", auth_views.LoginView.as_view(), name="login"),
     path("accounts/logout/", auth_views.LogoutView.as_view(), name="logout"),
